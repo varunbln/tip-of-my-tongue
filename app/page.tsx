400ca45d6b386fd09f9a2b770ab835dbe60b0501
varun-balani <varunprahladb@gmail.com>
2024-02-03 03:11:06 +0530

    bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import QueryForm from "@/components/QueryForm/QueryForm";
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen h-full w-full flex-col items-center justify-start">
+        <main className="flex h-full w-full flex-col items-center justify-start">
             <Header />
             <QueryForm />
         </main>
