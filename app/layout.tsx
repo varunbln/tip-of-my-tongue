11f46c4466968f994d75f856bcdf1920db2f631d
varun-balani <varunprahladb@gmail.com>
2024-02-05 15:31:46 +0530

    implement analytics with clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,18 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+            <head>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                       (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "kwynsq67e5");`,
+                    }}
+                />
+            </head>
             <body className={inter.className}>{children}</body>
         </html>
     );
